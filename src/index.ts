65e240fe2c610e9c5308880bb40098037979f39b
Deathspike <meister.deathspike@outlook.com>
2020-10-03 22:08:11 +0200

    Add series download support, CrunchyRoll-only

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+export * from './tools/browser';
 export * from './tools/episode';
+export * from './tools/series';
 export * from './settings';
 export * from './utilities';
 import './app';
---
+++ src/tools/evaluators/crunchyroll.js
@@ -0,0 +1,29 @@
+function crunchyroll() {
+  return Array.from(document.querySelectorAll('.season-dropdown')).reverse().map(seasonNode => ({
+    episodes: seasonNode.nextElementSibling ? extractEpisodes(seasonNode.nextElementSibling) : [],
+    title: reduceWhitespace(seasonNode.textContent)
+  }));
+
+  /**
+   * @param {Element} containerNode 
+   */
+  function extractEpisodes(containerNode) {
+    return Array.from(containerNode.querySelectorAll('a')).reverse().map(episodeNode => ({
+      title: reduceWhitespace(episodeNode.textContent),
+      url: episodeNode.href
+    }));
+  }
+
+  /**
+   * @param {string?} value
+   **/
+  function reduceWhitespace(value) {
+    return value ? value.trim().replace(/\s+/g, ' ') : '';
+  }
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = {crunchyroll};
+} else {
+  console.log(crunchyroll());
+}
