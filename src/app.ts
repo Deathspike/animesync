f1320be6d4067e88b5ed29ada290ab4505e5e5ff
Deathspike <>
2020-10-04 10:04:20 +0200

    Fix corruptible MKV output

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import * as app from '.';
 import path from 'path';
 
-// TODO: Output MKV into temp file then move, to prevent failures from re-downloading.
-// TODO: CloudFlare protection is not checked/bypassed on CrunchyRoll.
 // TODO: Logging? Either file-based or just console, but something, at least.
 // TODO: Add something resembling automation? Or a UI? At least prevent re-download after manual move.
 // TODO: Add login support for CrunchyRoll.
 // TODO: Region switching? Most anime isn't available in my region.
 // TODO: Funimation? Other sources?
+// TODO: CloudFlare protection is not checked/bypassed on CrunchyRoll.
 
 (async () => {
   const libraryPath = path.join(__dirname, '../lib');
