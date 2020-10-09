0a600c6f615c2ae95b730b1ae143ca4579f060f8
Deathspike <>
2020-10-09 10:42:21 +0200

    Launch browser w/ a tab for each streaming service

--- src/actions/browser.ts
+++ src/actions/browser.ts
@@ -3,5 +3,11 @@ import * as app from '..';
 export async function browserAsync() {
   console.log(`Launching browser ...`);
   app.settings.chromeHeadless = false;
-  await app.browserAsync((page) => new Promise((resolve) => page.context().on('close', resolve)));
+  await app.browserAsync(async (page) => {
+    const context = page.context();
+    const pages = context.pages();
+    if (pages.length > 0) pages[0].goto('https://www.crunchyroll.com/');
+    if (pages.length > 1) pages[1].goto('https://www.funimation.com/');
+    await new Promise((resolve) => context.on('close', resolve))
+  });
 }
