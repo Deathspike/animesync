9dcdd8d07ca8a24f38c5624afddde205d6016802
Deathspike <meister.deathspike@outlook.com>
2020-10-09 20:29:05 +0200

    Remove `find` function

--- src/classes/Sync.ts
+++ src/classes/Sync.ts
@@ -1,7 +1,7 @@
-import {find} from './utilities/find';
 import childProcess from 'child_process';
 import crypto from 'crypto';
 import fs from 'fs-extra';
+import os from 'os';
 import path from 'path';
 import util from 'util';
 
@@ -23,7 +23,7 @@ export class Sync {
     const fileNames = await fs.readdir(this._basePath);
     const filePaths = fileNames.map(fileName => path.join(this._basePath, fileName));
     const execLines = transform(filePaths.map(parse).sort(sort));
-    await util.promisify(childProcess.exec)(`${find('ffmpeg')} ${execLines} -c copy "${this._mergePath}"`);
+    await util.promisify(childProcess.exec)(`${ffmpeg()} ${execLines} -c copy "${this._mergePath}"`);
     await fs.move(this._mergePath, `${episodePath}.tmp`, {overwrite: true});
     await fs.move(`${episodePath}.tmp`, episodePath, {overwrite: true});
   }
@@ -31,7 +31,7 @@ export class Sync {
   async streamAsync(proxyServer: string, streamUrl: string) {
     const filePath = path.join(this._basePath, '.video.mp4');
     await fs.ensureDir(this._basePath);
-    await util.promisify(childProcess.exec)(`${find('ffmpeg')} -http_proxy "${proxyServer}" -i "${streamUrl}" -codec copy "${filePath}"`, {cwd: this._basePath});
+    await util.promisify(childProcess.exec)(`${ffmpeg()} -http_proxy "${proxyServer}" -i "${streamUrl}" -codec copy "${filePath}"`, {cwd: this._basePath});
   }
 
   async writeAsync(fileName: string, content: string) {
@@ -39,6 +39,11 @@ export class Sync {
     await fs.ensureDir(this._basePath);
     await fs.writeFile(filePath, content);
   }
+}
+
+function ffmpeg() {
+  if (os.platform() !== 'win32') return 'ffmpeg';
+  return path.join(__dirname, `../../dep/ffmpeg.exe`)
 }
 
 function parse(filePath: string) {
@@ -50,7 +55,7 @@ function parse(filePath: string) {
 
 function sort(item: ReturnType<typeof parse>, otherItem: ReturnType<typeof parse>) {
   if (item.extension === 'mp4') return -1;
-  if (item.language.startsWith('en')) return otherItem.extension === 'mp4' ? 1 : -1;
+  if (item.language === 'eng') return -1;
   return item.language.localeCompare(otherItem.language);
 }
 
