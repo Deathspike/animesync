c740004d363055de6b476352f0e22dcde103900b
Deathspike <meister.deathspike@outlook.com>
2021-01-16 20:47:07 +0100

    Fix browser spawned by CLI not closing

--- src/cli/actions/browser.ts
+++ src/cli/actions/browser.ts
@@ -4,13 +4,14 @@ export async function browserAsync() {
   console.log(`Starting ${ace.settings.serverUrl}`);
   await ace.Server.usingAsync(async (api) => {
     ace.settings.chromeHeadless = false;
+    ace.settings.chromeInactiveTimeout = 1000;
     console.log('Spawning browser ...');
     await api.get(ace.shr.BrowserService).pageAsync(async (page) => {
       const context = page.context();
       const pages = context.pages();
-      if (pages.length > 0) pages[0].goto('https://www.crunchyroll.com/');
-      if (pages.length > 1) pages[1].goto('https://www.funimation.com/');
-      await new Promise<void>((resolve) => context.on('close', resolve))
+      if (pages.length > 0) pages[0].goto('https://www.crunchyroll.com/').catch(() => undefined);
+      if (pages.length > 1) pages[1].goto('https://www.funimation.com/').catch(() => undefined);
+      await new Promise<void>((resolve) => context.on('close', resolve));
     });
   });
 }
