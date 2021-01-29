4042f68d26471f79429007d64ec558ea6c9b09bc
Deathspike <>
2021-01-29 15:17:39 +0100

    Fix remote API throwing on errors (#24)

--- src/api/remote/RemoteApi.ts
+++ src/api/remote/RemoteApi.ts
@@ -12,29 +12,35 @@ export class RemoteApi {
   async popularAsync(model: api.RemoteQueryPopular) {
     const query = querystring.stringify(model as any);
     const url = new URL(`/api/remote/popular?${query}`, this._baseUrl);
-    return await fetchJsonAsync<api.RemoteSearch>(url, 200);
+    return await fetchJsonAsync<api.RemoteSearch>(url);
   }
 
   async seriesAsync(model: api.RemoteQuerySeries) {
     const query = querystring.stringify(model as any);
     const url = new URL(`/api/remote/series?${query}`, this._baseUrl);
-    return await fetchJsonAsync<api.RemoteSeries>(url, 200);
+    return await fetchJsonAsync<api.RemoteSeries>(url);
   }
 
   async streamAsync(model: api.RemoteQueryStream) {
     const query = querystring.stringify(model as any);
     const url = new URL(`/api/remote/stream?${query}`, this._baseUrl);
-    return await fetchJsonAsync<api.RemoteStream>(url, 200);
+    return await fetchJsonAsync<api.RemoteStream>(url);
   }
 }
 
-async function fetchJsonAsync<T>(url: URL, ...status: Array<number>) {
-  const response = await fetch(url);
-  if (status.includes(response.status)) {
-    const value: T = await response.json();
-    return Object.assign(response, {value});
-  } else {
-    const value = undefined;
-    return Object.assign(response, {value});
+async function fetchJsonAsync<T>(url: URL) {
+  try {
+    const response = await fetch(url);
+    if (response.status === 200) {
+      const statusCode: 200 = 200;
+      const value: T = await response.json();
+      return {statusCode, value};
+    } else {
+      const statusCode = response.status;
+      return {statusCode};
+    }
+  } catch (error) {
+    const statusCode = 0;
+    return {statusCode};
   }
 }
