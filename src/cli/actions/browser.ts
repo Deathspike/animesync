289b2677b7d4d75a46b14e75265e58c9fd988512
Deathspike <>
2021-01-31 09:46:34 +0100

    Remove Server module override

--- src/cli/actions/browser.ts
+++ src/cli/actions/browser.ts
@@ -8,8 +8,8 @@ export async function browserAsync() {
     await api.browser.pageAsync(async (page) => {
       const context = page.context();
       const pages = context.pages();
-      if (pages.length > 0) pages[0].goto('https://www.crunchyroll.com/').catch(() => undefined);
-      if (pages.length > 1) pages[1].goto('https://www.funimation.com/').catch(() => undefined);
+      if (pages.length > 0) pages[0].goto('https://www.crunchyroll.com/').catch(() => {});
+      if (pages.length > 1) pages[1].goto('https://www.funimation.com/').catch(() => {});
       await new Promise<void>((resolve) => context.on('close', resolve));
     });
   });
