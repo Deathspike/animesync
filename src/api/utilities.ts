e688d4e6d3b010b94dad5e3734524bdecba84eaf
Deathspike <meister.deathspike@outlook.com>
2021-02-09 18:59:22 +0100

    Fix incorrect API calls and documentation

--- src/api/utilities.ts
+++ src/api/utilities.ts
@@ -1,5 +1,6 @@
 import {RequestInit} from 'node-fetch';
 import fetch from 'node-fetch';
+import querystring from 'querystring';
 
 export async function jsonAsync<T>(url: URL, init?: RequestInit) {
   try {
@@ -20,12 +21,17 @@ export async function jsonAsync<T>(url: URL, init?: RequestInit) {
   }
 }
 
+export function queryString(model?: Record<string, string>) {
+  if (!model) return querystring.stringify();
+  const result: Record<string, string> = {};
+  Object.entries(model).filter(([_, v]) => typeof v !== 'undefined').forEach(([k, v]) => result[k] = v);
+  return '?' + querystring.stringify(result);
+}
+
 export function property<T extends object, K extends keyof T, V extends T[K]>(name: K, source: T | undefined, sourcePatch: Partial<T> | undefined, value: V) {
-  return sourcePatch?.hasOwnProperty(name)
-    ? sourcePatch[name] as T[K]
-    : source?.hasOwnProperty(name)
-      ? source[name]
-      : value;
+  if (sourcePatch?.hasOwnProperty(name)) return sourcePatch[name] as T[K];
+  if (source?.hasOwnProperty(name)) return source[name];
+  return value;
 }
 
 export function unsafe<T>(value: T) {
