7aabe82bb07eacff22722d64b2db8e522113f3ca
Deathspike <>
2021-02-14 13:39:15 +0100

    Fix settings recycling context w/o changes

--- src/setting/services/SettingService.ts
+++ src/setting/services/SettingService.ts
@@ -17,18 +17,21 @@ export class SettingService {
   }
 
   async coreAsync(core: app.api.SettingCore) {
+    if (shallowEquals(app.settings.core, core)) return;
     app.settings.core = core;
     await this.restartAsync();
     await this.saveAsync();
   }
 
   async credentialAsync(credential: app.api.SettingCredential) {
+    if (shallowEquals(app.settings.credential, credential)) return;
     app.settings.credential = credential;
     await this.restartAsync();
     await this.saveAsync();
   }
 
   async pathAsync(path: app.api.SettingPath) {
+    if (shallowEquals(app.settings.path, path)) return;
     app.settings.path = path;
     await this.restartAsync();
     await this.saveAsync();
@@ -52,8 +55,14 @@ export class SettingService {
   }
 }
 
-function shallowDiff(current: Record<string, any>, b: Record<string, any>) {
+function shallowDiff(a: Record<string, any>, b: Record<string, any>) {
   const result: Record<string, any> = {};
-  Object.keys(current).filter(x => current[x] !== b[x]).forEach(x => result[x] = current[x]);
+  Object.keys(a).filter(x => a[x] !== b[x]).forEach(x => result[x] = a[x]);
   return result;
+}
+
+function shallowEquals(a: Record<string, any>, b: Record<string, any>) {
+  if (Object.keys(a).some(x => !(x in b) || a[x] !== b[x])) return false;
+  if (Object.keys(b).some(x => !(x in a) || a[x] !== b[x])) return false;
+  return true;
 }
