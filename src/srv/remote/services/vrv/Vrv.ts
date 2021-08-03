8a301453e394d623505f3c9520161fe814ee5df2
Deathspike <meister.deathspike@outlook.com>
2021-08-03 18:40:44 +0200

    Improve vrv with crunchyroll beta mechanisms

--- src/srv/remote/services/vrv/Vrv.ts
+++ src/srv/remote/services/vrv/Vrv.ts
@@ -2,7 +2,6 @@ import * as app from '../..';
 import * as ncm from '@nestjs/common';
 import * as vrv from './typings';
 import {evaluateNavigate} from './evaluators/navigate';
-import {evaluateNextSeason} from './evaluators/nextSeason';
 import {VrvCredential} from './VrvCredential';
 import {VrvRemap} from './VrvRemap';
 import playwright from 'playwright-core';
@@ -10,9 +9,11 @@ const baseUrl = 'https://vrv.co';
 
 @ncm.Injectable()
 export class Vrv implements app.IProvider {
+  private readonly agentService: app.AgentService;
   private readonly browserService: app.BrowserService;
 
-  constructor(browserService: app.BrowserService) {
+  constructor(agentService: app.AgentService, browserService: app.BrowserService) {
+    this.agentService = agentService;
     this.browserService = browserService;
   }
 
@@ -32,10 +33,10 @@ export class Vrv implements app.IProvider {
       await VrvCredential.tryAsync(baseUrl, page);
       const [episodesPromise, seasonsPromise, seriesPromise] = new app.Observer(page).getAsync(/\/-\/episodes$/, /\/-\/seasons$/, /\/-\/series\/[^\/]+$/);
       await page.evaluate(evaluateNavigate, seriesUrl);
-      const episodes = await episodesPromise.then(x => x.json() as Promise<vrv.Collection<vrv.Episode>>);
+      const seasonEpisodes: Array<vrv.Collection<vrv.Episode>> = [];
       const seasons = await seasonsPromise.then(x => x.json() as Promise<vrv.Collection<vrv.Season>>);
       const series = await seriesPromise.then(x => x.json() as Promise<vrv.Series>);
-      const seasonEpisodes = await fetchEpisodesAsync([episodes], page);
+      await this.fetchEpisodesAsync(episodesPromise, seasons, seasonEpisodes);
       const headers = Object.assign({'user-agent': userAgent}, defaultHeaders);
       const value = VrvRemap.series(seriesUrl, series, seasons, seasonEpisodes);
       return new app.Composable(seriesUrl, value, headers);
@@ -54,17 +55,21 @@ export class Vrv implements app.IProvider {
       return new app.Composable(streamUrl, value, headers);
     });
   }
-}
 
-async function fetchEpisodesAsync(episodes: Array<vrv.Collection<vrv.Episode>>, page: playwright.Page) {
-  while (true) {
-    const [episodesPromise] = new app.Observer(page).getAsync(/\/-\/episodes$/);
-    if (await page.evaluate(evaluateNextSeason)) {
-      episodes.push(await episodesPromise.then(x => x.json() as Promise<vrv.Collection<vrv.Episode>>));
-      continue;
-    } else {
-      episodesPromise.catch(() => {});
-      return episodes;
+  private async fetchEpisodesAsync(episodesPromise: Promise<playwright.Response>, seasons: vrv.Collection<vrv.Season>, seasonEpisodes: Array<vrv.Collection<vrv.Episode>>) {
+    const episodesResponse = await episodesPromise;
+    const episodesRequest = episodesResponse.request();
+    const episodesUrl = episodesRequest.url();
+    for (const season of seasons.items) {
+      const seasonUrl = episodesUrl.replace(/((\?|&)season_id=)[^&]+/, (_, x) => x + season.id);
+      if (episodesUrl !== seasonUrl) {
+        const headers = Object.entries(episodesRequest.headers()).filter(x => !x[0].startsWith(':'));
+        const buffer = await this.agentService.fetchAsync(new URL(seasonUrl), {headers: {...headers, ...defaultHeaders}});
+        seasonEpisodes.push(JSON.parse(buffer.toString('utf8')));
+      } else {
+        const episodes = await episodesResponse.json();
+        seasonEpisodes.push(episodes as vrv.Collection<vrv.Episode>);
+      }
     }
   }
 }
