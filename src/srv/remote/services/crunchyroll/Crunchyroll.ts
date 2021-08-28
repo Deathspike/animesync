2181e8373f10732ad372190d31145b30353065ef
Deathspike <meister.deathspike@outlook.com>
2021-08-28 19:58:23 +0200

    Fix Crunchyroll authentication race conditions

--- src/srv/remote/services/crunchyroll/Crunchyroll.ts
+++ src/srv/remote/services/crunchyroll/Crunchyroll.ts
@@ -58,11 +58,10 @@ async function tryLoginAsync(page: playwright.Page, url: string) {
   const isAuthenticated = () => Boolean(document.querySelector('.username'));
   if (!app.settings.credential.crunchyrollUsername || !app.settings.credential.crunchyrollPassword || await page.evaluate(isAuthenticated)) return;
   await page.goto(new URL('/login', url).toString(), {waitUntil: 'domcontentloaded'});
-  await page.click('#onetrust-accept-btn-handler', {timeout: 5000}).then(() => page.waitForNavigation({waitUntil: 'domcontentloaded'})).catch(() => {});
   await page.type('#login_form_name', app.settings.credential.crunchyrollUsername);
   await page.type('#login_form_password', app.settings.credential.crunchyrollPassword);
   await page.evaluate(() => document.querySelector('.opt-in')?.remove());
-  await page.click('#login_submit_button');
+  await page.click('#login_submit_button', {force: true});
   await page.waitForFunction(isAuthenticated);
   await page.goto(url, {waitUntil: 'domcontentloaded'});
 }
--- src/srv/remote/services/crunchyrollBeta/CrunchyrollBeta.ts
+++ src/srv/remote/services/crunchyrollBeta/CrunchyrollBeta.ts
@@ -83,11 +83,9 @@ async function tryLoginAsync(page: playwright.Page) {
   const isAuthenticated = () => Boolean(JSON.parse(localStorage.getItem('ajs_user_id') ?? 'null'));
   if (!app.settings.credential.crunchyrollUsername || !app.settings.credential.crunchyrollPassword || await page.evaluate(isAuthenticated)) return;
   await page.goto('https://www.crunchyroll.com/login', {waitUntil: 'domcontentloaded'});
-  await page.click('#onetrust-accept-btn-handler', {timeout: 5000}).then(() => page.waitForNavigation({waitUntil: 'domcontentloaded'})).catch(() => {});
   await page.type('#login_form_name', app.settings.credential.crunchyrollUsername);
   await page.type('#login_form_password', app.settings.credential.crunchyrollPassword);
-  await page.evaluate(() => document.querySelector('.opt-in')?.remove());
-  await page.click('#login_submit_button');
+  await page.click('#login_submit_button', {force: true});
   await page.waitForFunction(isAuthenticated);
 }
 
