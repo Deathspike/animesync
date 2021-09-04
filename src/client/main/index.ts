97e03b396480fb9f741491466922a46f3de96c34
Deathspike <meister.deathspike@outlook.com>
2021-09-04 10:41:08 +0200

    Reorganize web structure

--- src/client/main/index.ts
+++
@@ -1,6 +0,0 @@
-export * from '../shared';
-export * from './viewModels/MainSeriesViewModel';
-export * from './viewModels/MainViewModel';
-export * from './views/MainSeriesView';
-export * from './views/MainView';
-export * from './MainController';
