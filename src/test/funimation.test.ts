7313bb5a46a3497629cb252973c515ea8a96c422
Deathspike <meister.deathspike@outlook.com>
2021-09-06 18:31:21 +0200

    Fix Funimation test for recent provider bugs

--- src/test/funimation.test.ts
+++ src/test/funimation.test.ts
@@ -9,23 +9,23 @@ describe('Funimation', () => {
   }));
 
   it('Series', () => app.Server.usingAsync(async (api) => {
-    const series = await api.remote.seriesAsync({url: 'https://www.funimation.com/shows/blue-reflection-ray/'});
+    const series = await api.remote.seriesAsync({url: 'https://www.funimation.com/shows/the-saints-magic-power-is-omnipotent/'});
     expect(series.error).toBeUndefined();
     expect(series.value?.imageUrl).not.toBeUndefined();
     expect(series.value?.seasons).not.toEqual([]);
     expect(series.value?.synopsis).not.toBeUndefined();
-    expect(series.value?.title).toEqual('Blue Reflection Ray');
+    expect(series.value?.title).toEqual('The Saint\'s Magic Power is Omnipotent');
     expect(series.value?.url).not.toBeUndefined();
     expect(series.value?.seasons[0].title).toEqual('Season 1');
     expect(series.value?.seasons[0].episodes[0].imageUrl).not.toBeUndefined();
     expect(series.value?.seasons[0].episodes[0].name).toEqual('1');
     expect(series.value?.seasons[0].episodes[0].synopsis).not.toBeUndefined();
-    expect(series.value?.seasons[0].episodes[0].title).toEqual('The Undying Light');
+    expect(series.value?.seasons[0].episodes[0].title).toEqual('Summoning');
     expect(series.value?.seasons[0].episodes[0].url).not.toBeUndefined();
   }));
 
   it('Stream', () => app.Server.usingAsync(async (api) => {
-    const stream = await api.remote.streamAsync({url: 'https://www.funimation.com/en/shows/blue-reflection-ray/the-undying-light/'});
+    const stream = await api.remote.streamAsync({url: 'https://www.funimation.com/en/shows/the-saints-magic-power-is-omnipotent/summoning/'});
     expect(stream.error).toBeUndefined();
     expect(stream.value?.sources).not.toEqual([]);
     expect(stream.value?.subtitles).not.toEqual([]);
